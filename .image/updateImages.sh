e87916c5c6c039dbe6aff0a7e2e15d881eb62611
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-02-20 19:40:37 +0400

    Fixed updateImages.sh

--- .image/updateImages.sh
+++ .image/updateImages.sh
@@ -23,7 +23,7 @@ total=${#dirs[@]}
 let i=1
 for d in "${dirs[@]}"; do
     cd $d
-    #pdflatex -shell-escape image.tex > /dev/null 2>&1 && pdfcrop image.pdf image.pdf > /dev/null 2>&1 && pdf2svg image.pdf image.svg
+    pdflatex -shell-escape image.tex > /dev/null 2>&1 && pdfcrop image.pdf image.pdf > /dev/null 2>&1 && pdf2svg image.pdf image.svg
     echo "$d ($i/$total)"
     ((i=i+1))
     cd ..
