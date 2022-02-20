c2181a1f24dbb33aab551fac1689e393836929d2
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-02-20 16:25:58 +0400

    Testing commit script

--- .scripts/gitCommit.sh
+++ .scripts/gitCommit.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 CYAN='\033[0;36m'
+RED='\033[0;31m'
 NC='\033[0m'
 
 cd ~/MathWiki
 git status
 
-printf "\n"
 read -p "$(echo -e ${CYAN}"Commit? [Y/n] "${NC})" choice
 if [ -z "$choice" ] || [ "$choice" == "Y" ]; then
     git add .
-    read -p "$(echo -e ${CYAN}"Message: "${NC})" msg
-    git commit -m "$msg"
 
     printf "\n"
     git status
     printf "\n"
 
-    read -p "$(echo -e ${CYAN}"Remove files? [Y/n]"${NC})" choice
-    if [ -z $choice ]|| [ "$choice" == "Y" ]; then
+    read -p "$(echo -e ${CYAN}"Remove files? [N/(string)]"${NC})" choice
+    if [ ! -z $choice ]; then
         echo remove
     fi
+
+    read -p "$(echo -e ${CYAN}"Message: "${NC})" msg
 else
+    echo -e ${RED}Abort${NC}
     exit
 fi
