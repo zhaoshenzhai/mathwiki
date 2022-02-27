86a7a85db6f05cf7ec40d083647ba931723d2f9c
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-02-27 22:07:23 +0400

    Optimized updateMathLinks.sh

--- .scripts/updateMathLinks.sh
+++ .scripts/updateMathLinks.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
 CYAN='\033[0;36m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 
 cd ~/MathWiki/Notes
 
-files=$(grep -l "%%auto_aliasing%%" *)
+allLinks=$(grep -Po '\[(\w+\s)+\$\\[a-zA-Z^0-9]+\$[a-zA-Z(); ]*\]\(([a-zA-Z0-9();]+%20)+[a-zA-Z0-9();]*(\.md)*\)' * | sed 's/^.*://g' | uniq)
 
-while IFS= read -r line; do
-    current=$(grep -Po '\[(\w+\s)+\$\\[a-zA-Z^0-9]+\$[a-zA-Z(); ]*\]' "$line" | sed 's/^.*://g' | sed 's/\\/\\\\/g')
-    currentFormatted=$(sed 's/\\R/\\\\R/g' <<< "$current" | sed 's/\ /\\s/g' | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | sed 's/\$/\\\$/g')
+allCurrent=$(echo "$allLinks" | sed 's/\[\([^]]*\)\].*/\1/g')
+allNewObsidian=$(echo "$allLinks" | sed 's/\[\([^]]*\)\](\(.*$\)/\2/g' | sed 's/.$//g')
+allNew=$(echo "$allNewObsidian" | sed 's/\(.*\).md/\1/' | sed 's/%20/\ /g' | sed 's/\ R\ /\ \$\\R\$\ /g')
 
-    new=$(grep -Po '\(([a-zA-Z0-9();]+%20)+[a-zA-Z0-9();]*(\.md)*\)' "$line" | sed 's/%20/\ /g' | sed 's/(/[/1' | sed 's/)$/]/g' | sed 's/.md//g')
-
-    new=$(sed 's/\ R\ /\ \$\\\\R\$\ /g' <<< "$new")
+while IFS="," read -r current; do
+    new=${allNew%%$'\n'*}
+    newObsidian=${allNewObsidian%%$'\n'*}
 
     if [ ! "$new" == "$current" ]; then
-        echo "... $line ..."
-        echo "Current            : $current"
-        echo "New                : $new"
-        echo "Current (Formatted): $currentFormatted"
+        echo "Current: $current"
+        echo -e "New    : $new\n"
 
         read -u 1 -n 1 -p "$(echo -e ${CYAN}"Proceed? [Y/n] "${NC})" proceed
         if [ -z "$proceed" ] || [ "$proceed" == "Y" ]; then
-            echo "Proceed"
-            sed -Ei 's/'"$currentFormatted"'/'"$new"'/g' "$line"
+            currentFormatted=$(sed 's/\\R/\\\\R/g' <<< "$current" | sed 's/\ /\\s/g' | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | sed 's/\$/\\\$/g')
+            allFiles=$(grep -l "$newObsidian" *)
+            numFiles=$(echo "$allFiles" | wc -l)
+            new=$(echo "$new" | sed 's/\\/\\\\/g')
+
+            while IFS= read -r file; do
+                sed -Ei 's/'"$currentFormatted"'/'"$new"'/g' "$file"
+            done <<< "$allFiles"
+            echo -e "${GREEN}$numFiles" file\(s\) updated:"${NC}"
+            while IFS= read -r file; do
+                echo "    $file"
+            done <<< "$allFiles"
+
         fi
 
         printf "\n"
     fi
-done <<< "$files"
+
+    allNew=${allNew#*$'\n'}
+    allNewObsidian=${allNewObsidian#*$'\n'}
+done <<< "$allCurrent"
