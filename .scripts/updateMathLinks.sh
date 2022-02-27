7c2cd39aa891b24930095f1ba002d332306d3074
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-02-27 23:19:36 +0400

    Finished updateMathLinks.sh

--- .scripts/updateMathLinks.sh
+++ .scripts/updateMathLinks.sh
@@ -6,26 +6,46 @@ NC='\033[0m'
 
 cd ~/MathWiki/Notes
 
-allLinks=$(grep -Po '\[(\w+\s)+\$\\[a-zA-Z^0-9]+\$[a-zA-Z(); ]*\]\(([a-zA-Z0-9();]+%20)+[a-zA-Z0-9();]*(\.md)*\)' * | sed 's/^.*://g' | uniq)
+allLinks=$(grep -Po '\[([^\$^\[^\]]+\s)+\$[^\$]+\$[^\$^\[^\]]*\]\(([^\$^\[^\]]+%20)+[^\$^\[^\]]*(\.md)*\)' *)
+allLinks=${allLinks#*:}
+allLinks=$(echo "$allLinks" | uniq)
 
 allCurrent=$(echo "$allLinks" | sed 's/\[\([^]]*\)\].*/\1/g')
 allNewObsidian=$(echo "$allLinks" | sed 's/\[\([^]]*\)\](\(.*$\)/\2/g' | sed 's/.$//g')
-allNew=$(echo "$allNewObsidian" | sed 's/\(.*\).md/\1/' | sed 's/%20/\ /g' | sed 's/\ R\ /\ \$\\R\$\ /g')
+allNew=$(echo "$allNewObsidian" | sed 's/\(.*\).md/\1/' | sed 's/%20/\ /g')
+
+allNew=$(sed 's/\ R\ /\ \$\\R\$\ /g' <<< "$allNew")        # Real numbers
 
 while IFS="," read -r current; do
     new=${allNew%%$'\n'*}
-    newObsidian=${allNewObsidian%%$'\n'*}
 
     if [ ! "$new" == "$current" ]; then
         echo "Current: $current"
-        echo -e "New    : $new\n"
+        echo "New    : $new"
 
-        read -u 1 -n 1 -p "$(echo -e ${CYAN}"Proceed? [Y/n] "${NC})" proceed
+        read -u 1 -n 1 -p "$(echo -e '\n'${CYAN}"Proceed? [Y/n] "${NC})" proceed
         if [ -z "$proceed" ] || [ "$proceed" == "Y" ]; then
-            currentFormatted=$(sed 's/\\R/\\\\R/g' <<< "$current" | sed 's/\ /\\s/g' | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | sed 's/\$/\\\$/g')
+            currentFormatted=$(sed 's/\\/\\\\/g' <<< "$current")                 # Excape \
+            currentFormatted=$(sed 's/\ /\\s/g' <<< "$currentFormatted")         # Escape <Space>
+            currentFormatted=$(sed 's/\[/\\\[/g' <<< "$currentFormatted")        # Escape [
+            currentFormatted=$(sed 's/\]/\\\]/g' <<< "$currentFormatted")        # Escape ]
+            currentFormatted=$(sed 's/(/\\(/g' <<< "$currentFormatted")          # Escape (
+            currentFormatted=$(sed 's/)/\\)/g' <<< "$currentFormatted")          # Escape )
+            currentFormatted=$(sed 's/{/\\{/g' <<< "$currentFormatted")          # Escape {
+            currentFormatted=$(sed 's/}/\\}/g' <<< "$currentFormatted")          # Escape }
+            currentFormatted=$(sed 's/\$/\\\$/g' <<< "$currentFormatted")        # Escape $
+            currentFormatted=$(sed 's/\^/\\\^/g' <<< "$currentFormatted")        # Escape ^
+            currentFormatted=$(sed 's/|/\\|/g' <<< "$currentFormatted")          # Escape |
+            currentFormatted=$(sed 's/+/\\+/g' <<< "$currentFormatted")          # Escape +
+            currentFormatted=$(sed 's/\./\\./g' <<< "$currentFormatted")          # Escape +
+
+            newObsidian=${allNewObsidian%%$'\n'*}
             allFiles=$(grep -l "$newObsidian" *)
             numFiles=$(echo "$allFiles" | wc -l)
             new=$(echo "$new" | sed 's/\\/\\\\/g')
+
+            echo "Current Formatted: $currentFormatted"
+            echo "New: $new"
 
             while IFS= read -r file; do
                 sed -Ei 's/'"$currentFormatted"'/'"$new"'/g' "$file"
