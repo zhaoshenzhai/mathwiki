661898975cc794d6becfaa280db9b6353ed276cd
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-03-04 15:12:37 +0400

    Testing links

--- .github/gitCommit.sh
+++ .github/gitCommit.sh
@@ -40,7 +40,7 @@ if [ -z "$choice" ] || [ "$choice" == "Y" ]; then
     sed -i 's/[0-9][0-9]*\slinks/'"$numLinks"' links/g' README.md
     sed -i 's/[0-9][0-9]*\simages/'"$numImages"' images/g' README.md
 
-    templatesInsertLine=$(grep -n "#### Templates" vimwiki.snippets | sed 's/:.*$//g')
+    templatesInsertLine=$(grep -n "#### Templates" ~/.config/nvim/UltiSnips/vimwiki.snippets | sed 's/:.*$//g')
     sed -i 's|\[templatesInsert\]:.*$|\[templatesInsert\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/UltiSnips/vimwiki.snippets#L'"$templatesInsertLine"'|g' README.md
 
     git add .
