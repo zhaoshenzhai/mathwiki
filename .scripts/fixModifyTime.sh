3ecd8bd4c46d5b85a0d32d13d6f247f6f058ebb1
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-06-29 16:35:59 +0400

    Control update frequency

--- .scripts/fixModifyTime.sh
+++ .scripts/fixModifyTime.sh
@@ -10,6 +10,7 @@ cd ~/MathWiki/Notes
 
 allFiles=`ls`
 numberOfFiles=`echo "$allFiles" | wc -l`
+updateInterval=$((numberOfFiles/100))
 counter=0
 
 while IFS= read -r file; do
@@ -18,16 +19,16 @@ while IFS= read -r file; do
     year=`echo "$time" | sed 's/..\/..\///g' | sed 's/\ ..\:..\:..//g'`
     month=`echo "$time" | sed 's/^..\///g' | sed 's/\/.*//g'`
     day=`echo "$time" | sed 's/\/.*//g'`
-    hour=`echo "$time" | sed 's/^.*\ //g' | sed 's/\:.*//g'`
-    minute=`echo "$time" | sed 's/^.*\ //g' | sed 's/^..\://g' | sed 's/\:.*//g'`
-    second=`echo "$time" | sed 's/^.*\ //g' | sed 's/^..\:..\://g' | sed 's/\:.*//g'`
+    hms=`echo "$time" | sed 's/^.*\ //g' | sed 's/\://' | sed 's/\:/./'`
 
-    newTime=`echo "$year$month$day$hour$minute.$second"`
-    #touch -m -t "$newTime" "$file"
+    newTime=`echo "$year$month$day$hms"`
+    touch -m -t "$newTime" "$file"
 
     counter=$((counter + 1))
-    percentage=$(bc -l <<< 'scale=3; '"$counter"'/'"$numberOfFiles"''*100 | sed 's/00$//g')
-    echo -ne "${YELLOW}Updating... $percentage%${NC}\r"
+    if [[ $((counter%updateInterval)) = 0 ]]; then
+        percentage=$(bc -l <<< 'scale=2; '"$counter"'/'"$numberOfFiles"''*100 | sed 's/\.00$//g')
+        echo -ne "${YELLOW}Updating... $percentage%${NC}\r"
+    fi
 done <<< "$allFiles"
 echo -ne "\n"
 
