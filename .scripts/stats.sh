d230d52456a37966bc9bccefcc405b13450fb557
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-07-14 09:32:29 +0400

    Moved README updates to stats.sh -r

--- .scripts/stats.sh
+++ .scripts/stats.sh
@@ -37,6 +37,18 @@ while [ ! -z "$1" ]; do
             sed -i 's/[0-9][0-9]*\snotes/'"$numNotes"' notes/g' README.md
             sed -i 's/[0-9][0-9]*\slinks/'"$numLinks"' links/g' README.md
             sed -i 's/[0-9][0-9]*\simages/'"$numImages"' images/g' README.md
+
+            templatesInsertLine=$(grep -n "#### Templates" ~/.config/nvim/UltiSnips/markdown.snippets | sed 's/:.*$//g')
+            sed -i 's|\[templatesInsert\]:.*$|\[templatesInsert\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/UltiSnips/markdown.snippets#L'"$templatesInsertLine"'|g' README.md
+
+            tikzInsertLine=$(grep -n "newTikZ.sh" ~/.config/nvim/config/MathWiki.vim | sed 's/:.*$//g')
+            sed -i 's|\[tikzInsert\]:.*$|\[tikzInsert\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/config/MathWiki.vim#L'"$tikzInsertLine"'|g' README.md
+
+            pdfLaTeXExecuteLine=$(grep -n "pdflatex -shell-escape image.tex" ~/.config/nvim/config/MathWiki.vim | sed 's/:.*$//g')
+            sed -i 's|\[pdfLaTeXExecute\]:.*$|\[pdfLaTeXExecute\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/config/MathWiki.vim#L'"$pdfLaTeXExecuteLine"'|g' README.md
+
+            autoAliasLine=$(grep -n "Math()" ~/Dropbox/MathWiki/.scripts/mathLinks.sh | sed 's/:.*$//g')
+            sed -i 's|\[standardAlias\]:.*$|\[standardAlias\]: https://github.com/zhaoshenzhai/MathWiki/blob/master/.scripts/mathLinks.sh#L'"$autoAliasLine"'|g' README.md
         ;;
     esac
 shift
