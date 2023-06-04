cb844a14f668273128f9fc42ebfd7b0bd1934eb2
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2023-06-04 13:29:38 -0400

    Terminal colors

--- .scripts/resetModifyTime.sh
+++ .scripts/resetModifyTime.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-YELLOW='\033[0;33m'
-PURPLE='\033[0;35m'
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-BLUE='\033[0;34m'
-RED='\033[0;31m'
-NC='\033[0m'
-
 cd $MATHWIKI_DIR/Notes
 
 echo ""
