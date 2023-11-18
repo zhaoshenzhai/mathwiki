8e68b885725215255de0b16fd49b619986b15606
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2023-11-18 15:08:49 -0500

    obs: fixed `toggleDark.sh`

--- .scripts/toggleDark.sh
+++ .scripts/toggleDark.sh
@@ -5,7 +5,9 @@ cd $MATHWIKI_DIR
 if [[ ! -z $(grep 'obsidian' .obsidian/appearance.json) ]]; then
     sed -i 's/248, 248, 255/0, 0, 0/g' .obsidian/plugins/obsidian-admonition/data.json
     sed -i 's/obsidian/moonstone/g' .obsidian/appearance.json
+    sed -i 's/56b6c2/197ce6/g' .obsidian/appearance.json
 else
     sed -i 's/0, 0, 0/248, 248, 255/g' .obsidian/plugins/obsidian-admonition/data.json
     sed -i 's/moonstone/obsidian/g' .obsidian/appearance.json
+    sed -i 's/197ce6/56b6c2/g' .obsidian/appearance.json
 fi
