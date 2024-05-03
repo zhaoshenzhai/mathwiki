3bd47bf2cb1113c2be605103ba70ede5618ddb46
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2024-05-03 14:34:10 -0400

    site: more on preview

--- .site/static/js/preview.js
+++ .site/static/js/preview.js
@@ -1,17 +1,24 @@
 var frameContainer;
+var links;
 
 function preview(link) {
     frameContainer = document.getElementById("preview");
-    frameContainer.innerHTML="<iframe src=" + link + " id=previewFrame scrolling=no title=Preview></iframe>"
+    frameContainer.innerHTML="<iframe src=" + link + " id=previewFrame class=right scrolling=no title=Preview></iframe>"
+
+    links = document.getElementById("links");
+    links.style.display = "none";
 
     var frame = document.getElementById("previewFrame");
     frame.addEventListener("load", function() {
         var frameDoc = frame.contentDocument || frame.contentWindow.document;
         var frameContent = frameDoc.getElementById("content");
+
         frameContent.classList.remove("left");
+        frameDoc.getElementById("links").style.display = "none";
     });
 }
 
-function remove(link) {
-    // frameContainer.innerHTML="";
+function remove() {
+    frameContainer.innerHTML="";
+    links.style.display = "inline";
 }
