68d57e3f46aea07f70eef391b573b1b950519149
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2024-05-12 20:13:04 -0400

    site: dark mode

--- docs/js/darkMode.js
+++ docs/js/darkMode.js
@@ -19,8 +19,8 @@ const darkVals = {
     "fg":                  "rgb(240,240,240)",
     "link":                "rgb(86,182,194)",
     "link-hover":          "rgb(99,210,224)",
-    "nav-button-bg":       "rgba(0,0,0,0.5)",
-    "nav-button-hover-bg": "rgba(55,55,55,0.5)",
+    "nav-button-bg":       "rgb(200,200,200,0.2)",
+    "nav-button-hover-bg": "rgb(150,150,150,0.2)",
     "box-bg":              "rgba(255,255,255,0.05)",
     "quote":               "rgba(255,255,255,0.25)"
 };
@@ -28,13 +28,28 @@ const darkVals = {
 window.toggleDark = toggleDark;
 
 export function toggleDark() {
+    var icons = document.getElementsByClassName("icon");
+    var navButtons = document.getElementsByClassName("navButton");
+
     if (darkMode) {
         for(var [key, val] of Object.entries(lightVals)) {
             root.style.setProperty('--' + key, val);
         }
+        for (var i = 0; i < icons.length; i++) {
+            icons[i].style.filter = "";
+        }
+        for (var i = 0; i < navButtons.length; i++) {
+            navButtons[i].style.filter = "";
+        }
     } else {
         for(var [key, val] of Object.entries(darkVals)) {
             root.style.setProperty('--' + key, val);
+        }
+        for (var i = 0; i < icons.length; i++) {
+            icons[i].style.filter = "invert(100%)";
+        }
+        for (var i = 0; i < navButtons.length; i++) {
+            navButtons[i].style.filter = "invert(100%)";
         }
     }
 
