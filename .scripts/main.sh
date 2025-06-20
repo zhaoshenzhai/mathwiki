5e9a0c62fcbde14389a928d0ead759a076958645
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2025-06-20 10:32:32 -0400

    newNote.sh

--- .scripts/main.sh
+++ .scripts/main.sh
@@ -7,20 +7,28 @@ while [[ "$repeat" == "Y" ]]; do
     echo ""
 
     echo -e "${CYAN}Actions:${NC}"
-    echo -e "    ${CYAN}(1): Git${NC}"
-    echo -e "    ${CYAN}(2): Search${NC}"
-    echo -e "    ${CYAN}(3): Restore${NC}"
-    echo -e "    ${CYAN}(4): Ghost links${NC}"
-    echo -e "    ${CYAN}(5): Update images${NC}"
-    echo -e "    ${CYAN}(6): Publish notes${NC}"
+    echo -e "    ${CYAN}(1/g): Git${NC}"
+    echo -e "    ${CYAN}(2/n): New${NC}"
+    echo -e "    ${CYAN}(3/o): Open${NC}"
+    echo -e "    ${CYAN}(4)  : Search${NC}"
+    echo -e "    ${CYAN}(5)  : Restore${NC}"
+    echo -e "    ${CYAN}(6)  : Ghost links${NC}"
+    echo -e "    ${CYAN}(7)  : Update images${NC}"
+    echo -e "    ${CYAN}(8)  : Publish notes${NC}"
     echo ""
 
-    read -n 1 -ep "$(echo -e ${CYAN}"Select: [1|(1-6)]${NC} ")" action
-    re='^[0-9]+$'
-    if ( [[ $action =~ $re ]] ) && ( [ "$action" -ge "1" ] && [ "$action" -le "6" ] ) || [[ -z "$action" ]]; then
+    read -n 1 -ep "$(echo -e ${CYAN}"Select: [1|(1-8)]${NC} ")" action
+    num='^[0-9]+$'
+    if ( [[ $action =~ $num ]] && [ "$action" -ge "1" ] && [ "$action" -le "8" ] ) || [[ -z "$action" ]]; then
         if [[ -z "$action" ]]; then
             action="1"
         fi
+    elif [[ "$action" == "g" ]]; then
+        action="1"
+    elif [[ "$action" == "n" ]]; then
+        action="2"
+    elif [[ "$action" == "o" ]]; then
+        action="3"
     elif [[ "$action" == "q" ]]; then
         exit
     else
@@ -32,12 +40,14 @@ while [[ "$repeat" == "Y" ]]; do
     cd $MATHWIKI_DIR/Notes
 
     case $action in
-        "1") $DOTFILES_DIR/scripts/gitCommit.sh -r "MathWiki" ;;
-        "2") $MATHWIKI_DIR/.scripts/searchReplace.sh ;;
-        "3") $MATHWIKI_DIR/.scripts/gitRestore.sh ;;
-        "4") $MATHWIKI_DIR/.scripts/ghostLinks.sh ;;
-        "5") $MATHWIKI_DIR/.scripts/updateImages.sh -a -g ;;
-        "6") $MATHWIKI_DIR/.scripts/publish.sh ;;
+        1) $DOTFILES_DIR/scripts/gitCommit.sh -r "MathWiki" ;;
+        2) $MATHWIKI_DIR/.scripts/newNote.sh ;;
+        3) $MATHWIKI_DIR/.scripts/openNote.sh ;;
+        4) $MATHWIKI_DIR/.scripts/search.sh ;;
+        5) $MATHWIKI_DIR/.scripts/restore.sh ;;
+        6) $MATHWIKI_DIR/.scripts/ghost.sh ;;
+        7) $MATHWIKI_DIR/.scripts/updateImages.sh -a -g ;;
+        8) $MATHWIKI_DIR/.scripts/publish.sh ;;
     esac
 
     echo ""
