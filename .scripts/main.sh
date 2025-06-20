926a794c644b8469b63bd7b228cc4ca3d1d3863d
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2025-06-20 10:42:36 -0400

    mian

--- .scripts/main.sh
+++ .scripts/main.sh
@@ -7,28 +7,21 @@ while [[ "$repeat" == "Y" ]]; do
     echo ""
 
     echo -e "${CYAN}Actions:${NC}"
-    echo -e "    ${CYAN}(1/g): Git${NC}"
-    echo -e "    ${CYAN}(2/n): New${NC}"
-    echo -e "    ${CYAN}(3/o): Open${NC}"
-    echo -e "    ${CYAN}(4)  : Search${NC}"
-    echo -e "    ${CYAN}(5)  : Restore${NC}"
-    echo -e "    ${CYAN}(6)  : Ghost links${NC}"
-    echo -e "    ${CYAN}(7)  : Update images${NC}"
-    echo -e "    ${CYAN}(8)  : Publish notes${NC}"
+    echo -e "    ${CYAN}(1): Git${NC}"
+    echo -e "    ${CYAN}(2): New${NC}"
+    echo -e "    ${CYAN}(3): Search${NC}"
+    echo -e "    ${CYAN}(4): Restore${NC}"
+    echo -e "    ${CYAN}(5): Ghost links${NC}"
+    echo -e "    ${CYAN}(6): Update images${NC}"
+    echo -e "    ${CYAN}(7): Publish notes${NC}"
     echo ""
 
     read -n 1 -ep "$(echo -e ${CYAN}"Select: [1|(1-8)]${NC} ")" action
     num='^[0-9]+$'
-    if ( [[ $action =~ $num ]] && [ "$action" -ge "1" ] && [ "$action" -le "8" ] ) || [[ -z "$action" ]]; then
+    if ( [[ $action =~ $num ]] && [ "$action" -ge "1" ] && [ "$action" -le "7" ] ) || [[ -z "$action" ]]; then
         if [[ -z "$action" ]]; then
             action="1"
         fi
-    elif [[ "$action" == "g" ]]; then
-        action="1"
-    elif [[ "$action" == "n" ]]; then
-        action="2"
-    elif [[ "$action" == "o" ]]; then
-        action="3"
     elif [[ "$action" == "q" ]]; then
         exit
     else
@@ -42,12 +35,11 @@ while [[ "$repeat" == "Y" ]]; do
     case $action in
         1) $DOTFILES_DIR/scripts/gitCommit.sh -r "MathWiki" ;;
         2) $MATHWIKI_DIR/.scripts/newNote.sh ;;
-        3) $MATHWIKI_DIR/.scripts/openNote.sh ;;
-        4) $MATHWIKI_DIR/.scripts/search.sh ;;
-        5) $MATHWIKI_DIR/.scripts/restore.sh ;;
-        6) $MATHWIKI_DIR/.scripts/ghost.sh ;;
-        7) $MATHWIKI_DIR/.scripts/updateImages.sh -a -g ;;
-        8) $MATHWIKI_DIR/.scripts/publish.sh ;;
+        3) $MATHWIKI_DIR/.scripts/search.sh ;;
+        4) $MATHWIKI_DIR/.scripts/restore.sh ;;
+        5) $MATHWIKI_DIR/.scripts/ghost.sh ;;
+        6) $MATHWIKI_DIR/.scripts/updateImages.sh -a -g ;;
+        7) $MATHWIKI_DIR/.scripts/publish.sh ;;
     esac
 
     echo ""
